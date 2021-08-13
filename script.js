f4c3c1420a2afd87507f0a1145b5cddd42c9deae
unknown <olena.denysova88@gmail.com>
2021-08-13 11:37:04 +0200

    infinite scrolling

--- script.js
+++ script.js
@@ -15,21 +15,48 @@ async function getPosts() {
 //Show posts in DOM
 async function showPosts() {
     const posts = await getPosts();
-  
+
     posts.forEach(post => {
-      const postEl = document.createElement('div');
-      postEl.classList.add('post');
-      postEl.innerHTML = `
+        const postEl = document.createElement('div');
+        postEl.classList.add('post');
+        postEl.innerHTML = `
         <div class="number">${post.id}</div>
         <div class="post-info">
           <h2 class="post-title">${post.title}</h2>
           <p class="post-body">${post.body}</p>
         </div>
       `;
-  
-      postsContainer.appendChild(postEl);
+
+        postsContainer.appendChild(postEl);
     });
-  }
+}
+
+// Show loader & fetch more posts
+function showLoading() {
+    loading.classList.add('show');
+
+    setTimeout(() => {
+        loading.classList.remove('show');
+
+        setTimeout(() => {
+            page++;
+            showPosts();
+        }, 300);
+    }, 1000);
+}
+
 
 //Show initial posts
 showPosts();
+
+window.addEventListener('scroll', () => {
+    const {
+        scrollTop,
+        scrollHeight,
+        clientHeight
+    } = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+        showLoading();
+    }
+});
